387aa99561b982f83350897efb46c39ccf0de016
laamin <genrih.md@gmail.com>
2021-04-08 07:21:06 +0300

    Add postgres apapter

--- src/main.cr
+++ src/main.cr
@@ -1,15 +1,26 @@
 # Lets's image I make a blog
 require "kemal"
 require "json"
+require "pg"
+require "creto"
 
 module Blog
   VERSION = "0.1.0"
+
+  module Repo
+    extend Crecto::Repo
+
+    config do |conf|
+      conf.adapter = Crecto::Adapters::Postgres
+      conf.hostname = "localhost"
+      conf.database = "laam"
+    end
+  end
 
   get "/" do |ctx|
     ctx.response.content_type = "application/json"
     {name: "Лямин", age: 30}.to_json
   end
-  
+
   Kemal.run
 end
-
