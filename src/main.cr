ef3f34e883aac7e5ebe982dc1f82967518d2a55a
Лямин <genrih.md@gmail.com>
2021-04-09 09:05:39 +0300

    Куь скусещ

--- src/main.cr
+++ src/main.cr
@@ -2,20 +2,9 @@
 require "kemal"
 require "json"
 require "pg"
-require "crecto"
 
 module Blog
   VERSION = "0.1.0"
-
-  module Repo
-    extend Crecto::Repo
-
-    config do |conf|
-      conf.adapter = Crecto::Adapters::Postgres
-      conf.hostname = "localhost"
-      conf.database = "laam"
-    end
-  end
 
   get "/" do |ctx|
     ctx.response.content_type = "application/json"
